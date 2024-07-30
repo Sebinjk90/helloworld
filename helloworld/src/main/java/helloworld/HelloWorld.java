53009aeaaac25929ad21bc7e0463a4406eacac54
sebin <sebin@LAPTOP-CL23BE85.speedport.ip>
2024-07-30 20:23:05 +0530

    test commit for hello world

---
+++ helloworld/src/main/java/helloworld/HelloWorld.java
@@ -0,0 +1,20 @@
+package helloworld;
+
+public class HelloWorld {
+	
+	public void display() 
+	{
+	
+	
+		
+	System.out.println("Hello world to display , testing jenkins functionality");
+	
+	}
+	
+	public static void main(String[] args)
+	{
+	
+	HelloWorld obj =new HelloWorld();
+	obj.display();
+	}
+}
